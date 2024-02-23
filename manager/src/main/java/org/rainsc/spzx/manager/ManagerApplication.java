4b8146f0287f77dcefd9a36863fc8083fa8be055
RaInSLc <8990586+rainslc@user.noreply.gitee.com>
2024-02-23 21:04:01 +0800

    添加redis启动

--- manager/src/main/java/org/rainsc/spzx/manager/ManagerApplication.java
+++ manager/src/main/java/org/rainsc/spzx/manager/ManagerApplication.java
@@ -1,6 +1,8 @@
 package org.rainsc.spzx.manager;
 
+
 import org.rainsc.spzx.manager.properties.UserProperties;
+import org.rainsc.spzx.manager.serverUtils.RedisUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,6 +13,16 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"org.rainsc.spzx"})
 public class ManagerApplication {
     public static void main(String[] args) {
+        // 检查Redis是否正在运行  为杜绝前端不知道redis是否启动而进行疯狂重定向
+        boolean redisRunning = RedisUtil.isRedisRunning();
+
+        if (!redisRunning) {
+            // 如果Redis未启动，则启动Redis
+            System.out.println("Redis , 启动!");
+            RedisUtil.startRedis();
+        }else System.out.println("已经开过啦");
+
+        // 启动Spring Boot应用程序
         SpringApplication.run(ManagerApplication.class);
         System.out.println("http://127.0.0.1:8501/doc.html");
     }
---
+++ manager/src/main/java/org/rainsc/spzx/manager/serverUtils/RedisUtil.java
@@ -0,0 +1,34 @@
+package org.rainsc.spzx.manager.serverUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RedisUtil {
+    // 检查Redis是否正在运行
+    public static boolean isRedisRunning() {
+        try {
+            Process process = Runtime.getRuntime().exec("tasklist");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("redis-server.exe")) {
+                    return true;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // 启动Redis
+    public static void startRedis() {
+        try {
+            Runtime.getRuntime().exec("E:\\Program Files\\Redis\\redis-server.exe");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
