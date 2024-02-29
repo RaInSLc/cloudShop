d38b72cbe337251c6a1f1079011d6a58b2197e8e
RaInSLc <8990586+rainslc@user.noreply.gitee.com>
2024-02-29 21:23:20 +0800

    引入测试

--- manager/src/main/java/org/rainsc/spzx/manager/config/WebMvcConfiguration.java
+++ manager/src/main/java/org/rainsc/spzx/manager/config/WebMvcConfiguration.java
@@ -1,14 +1,18 @@
 package org.rainsc.spzx.manager.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.rainsc.spzx.manager.Interceptor.LoginAuthInterceptor;
 import org.rainsc.spzx.manager.properties.UserProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 跨域请求处理
+@Slf4j
 @Component
 public class WebMvcConfiguration implements WebMvcConfigurer {
     @Autowired
@@ -17,13 +21,14 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     private UserProperties userProperties;
 
     // 跨域请求放行
+    //跨域
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")      // 添加路径规则
                 .allowCredentials(true)               // 是否允许在跨域的情况下传递Cookie
                 .allowedOriginPatterns("*")           // 允许请求来源的域规则
                 .allowedMethods("*")
-                .allowedHeaders("*");                // 允许所有的请求头
+                .allowedHeaders("*") ;
     }
 
     // 拦截器注册
@@ -33,8 +38,8 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 //                .excludePathPatterns("/admin/system/index/login"
 //                        , "/admin/system/index/generateValidateCode")
                 .excludePathPatterns(userProperties.getNoAuthUrls())
+//                .excludePathPatterns("/**")
                 .addPathPatterns("/**");
-
-
+        log.info("{}",userProperties.getNoAuthUrls());
     }
 }
