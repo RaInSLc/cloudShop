a26639154bb1000da37cbdad0f6e6d711c1ecd6e
RaInSLc <8990586+rainslc@user.noreply.gitee.com>
2024-03-01 22:56:39 +0800

    排除swagger相关请求

--- manager/src/main/java/org/rainsc/spzx/manager/config/WebMvcConfiguration.java
+++ manager/src/main/java/org/rainsc/spzx/manager/config/WebMvcConfiguration.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.rainsc.spzx.manager.Interceptor.LoginAuthInterceptor;
 import org.rainsc.spzx.manager.properties.UserProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,18 +26,41 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .allowCredentials(true)               // 是否允许在跨域的情况下传递Cookie
                 .allowedOriginPatterns("*")           // 允许请求来源的域规则
                 .allowedMethods("*")
-                .allowedHeaders("*") ;
+                .allowedHeaders("*");
     }
 
     // 拦截器注册
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 放行swagger
+        String[] excludePatterns = new String[]{"/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**",
+                "/api", "/api-docs", "/api-docs/**", "/doc.html/**"};
+
+
         registry.addInterceptor(loginAuthInterceptor)
 //                .excludePathPatterns("/admin/system/index/login"
 //                        , "/admin/system/index/generateValidateCode")
+//                .excludePathPatterns(excludePatterns)
                 .excludePathPatterns(userProperties.getNoAuthUrls())
 //                .excludePathPatterns("/**")
+//不拦截路由
+                .excludePathPatterns("/**/login")
+                .excludePathPatterns("/doc.html/**")
+                .excludePathPatterns("/doc.html#/**")
+                .excludePathPatterns("/swagger-ui.html")
+
+                //让后台文档不可用可以直接注释掉这两个v2 v3
+                .excludePathPatterns("/v2/api-docs/**")    // swagger api json
+                .excludePathPatterns("/v3/api-docs/**")
+                .excludePathPatterns("/swagger-resources/configuration/ui")// 用来获取支持的动作
+                .excludePathPatterns("/swagger-resources")       // 用来获取api-docs的URI
+                .excludePathPatterns("/swagger-resources/configuration/security")    // 安全选项
+                .excludePathPatterns("/swagger-resources/**")
+                //补充路径，近期在搭建swagger接口文档时，通过浏览器控制台发现该/webjars路径下的文件被拦截，故加上此过滤条件即可。(2020-10-23)
+                .excludePathPatterns("/webjars/**")
+                .excludePathPatterns("/error")
                 .addPathPatterns("/**");
-        log.info("{}",userProperties.getNoAuthUrls());
+
+//        log.info("{}", userProperties.getNoAuthUrls());
     }
 }
