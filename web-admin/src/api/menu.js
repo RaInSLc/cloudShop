95e381b516aec016726601bd926ac6ad5c21ddd0
RaInSLc <8990586+rainslc@user.noreply.gitee.com>
2024-03-11 21:08:32 +0800

    前端根据权限动态展示菜单

--- web-admin/src/api/menu.js
+++ web-admin/src/api/menu.js
@@ -13,9 +13,9 @@ import request from '@/utils/request'
 
 // 获取菜单
 export const GetMenus = params => {
-  return request({
-    url: '/api/menus',
-    method: 'get',
-    params,
-  })
+    return request({
+        url: '/admin/system/index/menus',
+        method: 'get',
+        params,
+    })
 }
