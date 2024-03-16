cb9733b5f32c61afe3166b76f8538d93d311f903
RaInSLc <8990586+rainslc@user.noreply.gitee.com>
2024-03-16 20:58:50 +0800

    perf: 用更高效的clear docs: 详细说明

--- manager/src/main/java/org/rainsc/spzx/manager/listener/ExcelListener.java
+++ manager/src/main/java/org/rainsc/spzx/manager/listener/ExcelListener.java
@@ -1,6 +1,5 @@
 package org.rainsc.spzx.manager.listener;
 
-
 // 监听器
 // spring是单例模式, 并发时如果同时读取, 则无法区分listener归属
 // 所以不能由spring管理 只能通过构造方法传递
@@ -8,50 +7,52 @@ package org.rainsc.spzx.manager.listener;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.listener.ReadListener;
 import com.alibaba.excel.util.ListUtils;
-import org.apache.poi.ss.formula.functions.T;
 import org.rainsc.spzx.manager.Mapper.CategoryMapper;
+import org.rainsc.spzx.model.vo.product.CategoryExcelVo;
 
 import java.util.List;
 
-public class ExcelListener implements ReadListener<T> {
+public class ExcelListener<T> implements ReadListener<T> {
 
     // 通过构造方法传递mapper
-    private CategoryMapper categoryMapper;
+    private final CategoryMapper categoryMapper;
 
+    // 构造方法
     public ExcelListener(CategoryMapper categoryMapper) {
         this.categoryMapper = categoryMapper;
     }
 
     // 创建缓存 防止频繁写入ＯＯＭ
-    // 缓存的数据量BATCH_COUNT
-
+    // 缓存的数据量 BATCH_COUNT
     private static final int BATCH_COUNT = 100;
 
+    // 缓存数据列表
     private List<T> cacheDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
 
     // 从第二行开始读取  每行封装到T中
     @Override
     public void invoke(T t, AnalysisContext analysisContext) {
-
+        // 将数据加入缓存列表
         cacheDataList.add(t);
         // 达到100 存入数据库
         if (cacheDataList.size() >= BATCH_COUNT) {
             // 批量存入数据库
             saveData();
-            // 清理list
-            cacheDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+            // 清理缓存列表
+            cacheDataList.clear();
         }
     }
 
-
+    // 读取完成后的操作
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
-        // 保存数据
+        // 保存剩余的数据
         saveData();
     }
 
-    // 保存方法
+    // 批量保存数据到数据库
     private void saveData() {
-        categoryMapper.batchInsert(cacheDataList);
+        // 强制类型转换为 CategoryExcelVo，并调用 Mapper 的批量插入方法
+        categoryMapper.batchInsert((List<CategoryExcelVo>) cacheDataList);
     }
 }
