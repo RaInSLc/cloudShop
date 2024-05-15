3da4653ab56acb6e6495065d3b46e24d63ad1a02
RaInSLc <8990586+rainslc@user.noreply.gitee.com>
2024-05-16 01:09:24 +0800

    feta: 畅销商品列表

--- service/service-product/src/main/java/org/rainsc/spzx/product/service/ProductService.java
+++ service/service-product/src/main/java/org/rainsc/spzx/product/service/ProductService.java
@@ -1,4 +1,9 @@
 package org.rainsc.spzx.product.service;
 
+import org.rainsc.spzx.model.entity.product.ProductSku;
+
+import java.util.List;
+
 public interface ProductService {
+    List<ProductSku> findProductSkuBySale();
 }
--- service/service-product/src/main/java/org/rainsc/spzx/product/service/impl/ProductServiceImpl.java
+++ service/service-product/src/main/java/org/rainsc/spzx/product/service/impl/ProductServiceImpl.java
@@ -1,7 +1,18 @@
 package org.rainsc.spzx.product.service.impl;
 
+import org.rainsc.spzx.model.entity.product.ProductSku;
+import org.rainsc.spzx.product.service.ProductService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
+    /**
+     * @return
+     */
+    @Override
+    public List<ProductSku> findProductSkuBySale() {
+        return null;
+    }
 }
