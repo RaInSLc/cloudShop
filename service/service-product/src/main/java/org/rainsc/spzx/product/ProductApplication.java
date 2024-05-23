59283971cf1d0d291b0b336019c7d22f451ace0f
RaInSLc <8990586+rainslc@user.noreply.gitee.com>
2024-05-24 01:52:47 +0800

    add: 引入redis缓存, 将一级category放入redis缓存

--- service/service-product/src/main/java/org/rainsc/spzx/product/ProductApplication.java
+++ service/service-product/src/main/java/org/rainsc/spzx/product/ProductApplication.java
@@ -1,5 +1,6 @@
 package org.rainsc.spzx.product;
 
+import org.rainsc.spzx.manager.serverUtils.RedisUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductApplication {
 
     public static void main(String[] args) {
+        // 检查Redis是否正在运行  为杜绝前端不知道redis是否启动而进行疯狂重定向
+        boolean redisRunning = RedisUtil.isRedisRunning();
+
+        if (!redisRunning) {
+            // 如果Redis未启动，则启动Redis
+            System.out.println("Redis , 启动!");
+            RedisUtil.startRedis();
+        }else System.out.println("已经开过啦");
         SpringApplication.run(ProductApplication.class, args);
     }
 
--- service/service-product/src/main/java/org/rainsc/spzx/product/service/impl/CategoryServiceImpl.java
+++ service/service-product/src/main/java/org/rainsc/spzx/product/service/impl/CategoryServiceImpl.java
@@ -1,24 +1,49 @@
 package org.rainsc.spzx.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import org.rainsc.spzx.model.entity.product.Category;
 import org.rainsc.spzx.product.mapper.CategoryMapper;
 import org.rainsc.spzx.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
 
     /**
      * @return
      */
     @Override
     public List<Category> findOneCategory() {
-        return categoryMapper.findOneCategory();
+        // 查询Redis，检查是否存在一级分类
+        String categoryOneJson = redisTemplate.opsForValue().get("category:firstC");
+        // 如果Redis中存在一级分类，则直接返回
+        if (StringUtils.hasText(categoryOneJson)) {
+            // 将Redis中的JSON转换为List<Category>并返回
+            List<Category> existCategoryList = JSON.parseArray(categoryOneJson, Category.class);
+            return existCategoryList;
+        }
+
+        // 如果Redis中不存在一级分类，则从数据库查询，并将结果缓存到Redis中
+        List<Category> categoryList = categoryMapper.findOneCategory();
+        // 将查询结果转换为JSON字符串
+        String jsonString = JSON.toJSONString(categoryList);
+        // 将结果存入Redis，设置缓存有效期为7天
+        redisTemplate.opsForValue().set("category:firstC", jsonString, 7, TimeUnit.DAYS);
+
+//        return categoryMapper.findOneCategory();
+        // 返回redis缓存中的内容
+        return categoryList;
     }
 
     /**
@@ -58,4 +83,6 @@ public class CategoryServiceImpl implements CategoryService {
         // 返回包含完整分类树的一级分类列表
         return firstCategoryList;
     }
+
+
 }
